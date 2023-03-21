78e1ce09d3eeb479ed515d9c5241cc23745301ea
Blake Alderink <blake.alderink@gmail.com>
2023-03-21 01:03:53 -0700

    added get request

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const server = require('./api/server');
-
 const port = 9000;
 
+const User = require('./api/users/model')
+
+
 // START YOUR SERVER HERE
+server.get('/api/users', (req, res) => {
+    User.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+           message: "error getting users",
+           err: err.message
+        })
+    });
+ 
+  });
+
+
+
+
+
+
+
+server.listen(port, () => {console.log(`API running on port ${port}`)})
